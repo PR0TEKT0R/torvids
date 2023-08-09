e2bfb5d3b02ce9c1eb792828a46e446cc0c3f0d9
Protektor <protektor@protektor.com>
2023-08-09 13:26:50 +0300

    Added my project

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from 'cors'
+import logger from 'morgan'
+import * as path from 'path'
+
+// router
+import streamRouter from './modules/stream/stream.controller'
+import contentRouter from './modules/content/content.controller'
+
+
+//middleware
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+//app.set('views', path.join(__dirname, 'views'))
+
+
+//endpoints
+app.use('/stream', streamRouter)
+app.use('/content', contentRouter)
+
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log('Начинаем стримить')
+    console.log(`http://localhost:${PORT}`)
+})
